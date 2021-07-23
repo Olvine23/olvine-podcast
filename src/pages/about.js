2331b4367ba6bf723d7360dcd03ba39da42b569e
Olvine George <olivinegeorge@gmail.com>
2021-07-23 11:02:50 +0300

    second commit

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const About = () => {
+    return (
+    <>
+    <h1>About Me</h1>
+    <p>This is a page that describes Me</p>
+    <h3>Get In Touch</h3>
+    </>
+    );
+};
+export default About;
