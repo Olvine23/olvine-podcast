7b99973035eeb212120b93fa7c6451c4a8b4aef1
Olvine George <olivinegeorge@gmail.com>
2021-07-25 13:00:41 +0300

    committing done

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Layout from '../components/layout'
+import {StaticImage} from 'gatsby-plugin-image'
 import {Link} from 'gatsby'
 
 
@@ -8,6 +9,8 @@ const About = () => {
     <Layout pageHeading = 'About' pageHeading = 'About Me' >
     <p>This is a page that describes Me</p>
     <p>Olvine George</p>
+    <StaticImage placeholder = "blurred" src ="../images/olvine.JPG" height= {500} width ={700} />
+
     <h3>Get In Touch</h3>
     <Link to = "/">Back to home</Link>
     </Layout>
